7d189b425e95ea6c249988408acf197418309ee1
Koki Rizov <kalo282828@gmail.com>
2024-08-07 14:46:25 +0300

    I made the getHumanChoise function and initialized varables for keeping the scores.

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //Simple function to get the computer choice.
 function computerChoice() {
-    let answer = Math.floor((Math.random() * 3) + 1);
-    return answer;
+    let random = Math.floor((Math.random() * 3) + 1);
+    
+    if (random == 1){
+      return "rock";
+    }
+    else if(random == 2)
+    {
+      return "paper";
+    }
+    else{
+        return "scissors";
+    }
+    
+    
 }
-
 console.log(computerChoice());
+//Simple function to get human answer.
+function getHumanChoice(){
+    let humanAnswer = prompt("What is your choice?");
+    
+    return humanAnswer;
+}
+// Variables for keeping the score of the game
+let humanScore = 0;
+let computerScore = 0;
